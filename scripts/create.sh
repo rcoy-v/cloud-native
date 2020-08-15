3ee580d8d78dbb22ecaffe0030056c4e1bca988d
rcoy-v <rcoy.vance@pm.me>
2020-08-15 14:25:19 -0500

    Make create idempotent; prettify script messages

--- scripts/create.sh
+++ scripts/create.sh
@@ -9,14 +9,23 @@ popd () {
     command popd "$@" > /dev/null
 }
 
+msg() {
+    printf "\n--- $1 ---\n\n"
+}
+
+msg 'Starting create script'
+
 oci session authenticate --region $HOME_REGION
 
+msg 'Applying Terraform'
 pushd tf
 terraform init
 terraform apply --auto-approve -var "tenancy_ocid=$TENANCY_OCID"
 CLUSTER_OCID=$(terraform output -json | jq -r '.cluster_ocid.value')
 popd
+msg 'Finished Terraform'
 
+msg 'Configuring access to OKE cluster'
 oci --auth security_token ce cluster create-kubeconfig \
     --cluster-id "$CLUSTER_OCID" \
     --overwrite
@@ -26,6 +35,7 @@ oci --auth security_token ce cluster create-kubeconfig \
 cat << EOF > /root/.kube/config
 $(yq r -j /root/.kube/config | jq '.users[0].user.exec.args |= . + ["--auth", "security_token"]' | yq r -P -)
 EOF
+msg 'Finished configuring access to OKE cluster'
 
 # Wait for OKE worker nodes to become ready.
 until [ $(kubectl get nodes | tail -n +2 | awk '{print $2}' | grep -e '^Ready$' | wc -l) == "2" ]; do
@@ -33,41 +43,59 @@ until [ $(kubectl get nodes | tail -n +2 | awk '{print $2}' | grep -e '^Ready$' 
     sleep 15
 done
 
+msg 'Preparing Helm charts'
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo add jetstack https://charts.jetstack.io
 helm repo add stable https://kubernetes-charts.storage.googleapis.com
 helm repo update
+msg 'Finished Helm preparation'
 
 pushd k8s
 
-helm install ingress-nginx ingress-nginx/ingress-nginx \
+msg 'Installing ingress-nginx'
+helm upgrade ingress-nginx ingress-nginx/ingress-nginx \
     -n ingress-nginx \
     --create-namespace \
     --version 2.11.2 \
+    --install \
     --wait \
     -f ingress-nginx/values.yaml
+msg 'Finished ingress-nginx'
 
-helm install cert-manager jetstack/cert-manager \
-    -n cert-manager \
-    --create-namespace \
-    --version v0.16.1 \
-    --wait \
-    -f cert-manager/values.yaml
+if ! helm status cert-manager -n cert-manager &> /dev/null; then
+    msg 'Installing cert-manager'
+    helm install cert-manager jetstack/cert-manager \
+        -n cert-manager \
+        --create-namespace \
+        --version v0.16.1 \
+        --wait \
+        -f cert-manager/values.yaml
+    msg 'Finished cert-manager'
+else
+    msg 'cert-manager already installed; skipping'
+fi
 
-helm install openfaas ./openfaas \
+msg 'Installing openfaas'
+helm upgrade openfaas ./openfaas \
   -n openfaas \
   --create-namespace \
+  --install \
   --wait
+msg 'Finished openfaas'
 
-helm install grafana stable/grafana \
+msg 'Installing grafana'
+helm upgrade grafana stable/grafana \
   -n grafana \
   --create-namespace \
   --version 5.5.5 \
+  --install \
   --wait \
   -f grafana/values.yaml
+msg 'Finished openfaas'
 
 popd
 
+msg 'Gathering load balancer and gateway information'
 # Setup local hosts entry for external LoadBalancer IP
 OPENFAAS_GATEWAY_IP=""
 while [ -z $OPENFAAS_GATEWAY_IP ]; do
@@ -80,14 +108,22 @@ echo "$OPENFAAS_GATEWAY_IP gateway.example" >> /etc/hosts
 # Trust self-signed cert when interacting with OpenFaaS gateway
 kubectl -n openfaas get secret openfaas-tls -o "jsonpath={.data['tls\.crt']}" | base64 -d > /usr/local/share/ca-certificates/openfaas.crt
 update-ca-certificates
+msg 'Finished gathering load balancer and gateway information'
 
+msg 'Deploying app function'
 OPENFAAS_USER=$(kubectl -n openfaas get secret basic-auth -o jsonpath="{.data.basic-auth-user}" | base64 -d)
 OPENFAAS_PASSWORD=$(kubectl -n openfaas get secret basic-auth -o jsonpath="{.data.basic-auth-password}" | base64 -d)
 
 echo $OPENFAAS_PASSWORD | faas-cli login -g https://gateway.example -u $OPENFAAS_USER -s
 faas-cli template pull
 faas-cli deploy -f app.yaml -g https://gateway.example
+msg 'Finished deploying app function'
 
-echo "Add '$OPENFAAS_GATEWAY_IP gateway.example' to your local hosts file"
-echo "Then access app at https://gateway.example/function/app"
-echo "You can also sign in to the OpenFaas management console at https://gateway.example with the credentials $OPENFAAS_USER:$OPENFAAS_PASSWORD"
+cat << EOF
+Add '$OPENFAAS_GATEWAY_IP gateway.example' to your local hosts file
+Then access app at https://gateway.example/function/app
+
+You can also sign in to the OpenFaas management console at https://gateway.example with the credentials $OPENFAAS_USER:$OPENFAAS_PASSWORD
+EOF
+
+msg 'Finished create script'
--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -9,8 +9,15 @@ popd () {
     command popd "$@" > /dev/null
 }
 
+msg() {
+    printf "\n--- $1 ---\n\n"
+}
+
+msg 'Starting destroy script'
+
 oci session authenticate --region $HOME_REGION
 
+msg 'Configuring access to OKE cluster'
 pushd tf
 CLUSTER_OCID=$(terraform output -json | jq -r '.cluster_ocid.value')
 popd
@@ -24,24 +31,41 @@ oci --auth security_token ce cluster create-kubeconfig \
 cat << EOF > /root/.kube/config
 $(yq r -j /root/.kube/config | jq '.users[0].user.exec.args |= . + ["--auth", "security_token"]' | yq r -P -)
 EOF
+msg 'Finished configuring access to OKE cluster'
+
 
 # Delete Kubernetes resources before destroying the OKE cluster, e.g. LoadBalancer services.
 # OKE will not clean up any dynamically created infrastructure when a cluster is removed.
 # This can prevent Terraform managed resources from being destroyed.
 pushd k8s
+
+msg 'Removing openfaas'
 helm uninstall openfaas -n openfaas
 kubectl delete ns openfaas
+msg 'Finished removing openfaas'
+
+msg 'Removing grafana'
 helm uninstall grafana -n grafana
 kubectl delete ns grafana
+msg 'Finished removing grafana'
+
+msg 'Removing ingress-nginx'
 helm uninstall ingress-nginx -n ingress-nginx
 kubectl delete ns ingress-nginx
+msg 'Finished removing ingress-nginx'
+
+msg 'Removing cert-manager'
 helm uninstall cert-manager -n cert-manager
 kubectl delete ns cert-manager
+msg 'Finished removing cert-manager'
+
 popd
 
+msg 'Destroying Terraform'
 pushd tf
 terraform init
 terraform destroy --auto-approve -var "tenancy_ocid=$TENANCY_OCID"
 popd
+msg 'Finished Terraform'
 
-echo "Everything has been successfully destroyed."
+msg 'Finished destroy script'
